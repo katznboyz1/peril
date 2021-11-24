05b24650ad06513cc33a666be702310972a01098
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-24 12:24:48 -0500

    clarified the output format for the templates

--- cpp_src/fetch_data_LANG_TEMPLATE.cpp
+++ cpp_src/fetch_data_LANG_TEMPLATE.cpp
@@ -10,6 +10,7 @@ using namespace std;
     Do all of the language data downloading here
     Use `<curl/curl.h>` to download the language data from the internet, as it already exists
     Do NOT modify other languages' data, only modify the language data associated with this files fetch_data_LANG_XXX.hpp file!
+    This should write the data to the file in the format of "question;answer" for every line.
 */
 int fetch_data_LANG_TEMPLATE_all(const std::string &category_data_path, const std::string &tmp_download_path, const int &curl_timeout_seconds) {
 
