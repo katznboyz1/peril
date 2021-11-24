1778a29ed8e3455e7d4b0ce9cc0550a4b29bf132
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-24 12:07:24 -0500

    added 'completed' labels to fetch_data_LANG_FR.hpp

--- cpp_src/fetch_data_LANG_FR.hpp
+++ cpp_src/fetch_data_LANG_FR.hpp
@@ -2,11 +2,11 @@
 
 // this should not be a relative or absolute path, it should just be the filename of the output file
 // should be in the format "data.CATEGORY_NAME.lang.LANGUAGE_CODE"
-const std::string LANG_FR_DATA_VOCAB_NOUNS = "data.vocab_nouns.lang.fr.txt";
-const std::string LANG_FR_DATA_VOCAB_ADJECTIVES = "data.vocab_adjectives.lang.fr.txt";
+const std::string LANG_FR_DATA_VOCAB_NOUNS = "data.vocab_nouns.lang.fr.txt"; // completed
+const std::string LANG_FR_DATA_VOCAB_ADJECTIVES = "data.vocab_adjectives.lang.fr.txt"; // completed
 const std::string LANG_FR_DATA_PHRASES = "data.phrases.lang.fr.txt";
 const std::string LANG_FR_DATA_VERB_CONJUGATIONS = "data.verb_conjugations.lang.fr.txt";
-const std::string LANG_FR_DATA_VERB_VOCABULARY = "data.verb_vocabulary.lang.fr.txt";
+const std::string LANG_FR_DATA_VERB_VOCABULARY = "data.verb_vocabulary.lang.fr.txt"; // completed
 const std::string LANG_FR_DATA_SIMPLE_TRIVIA = "data.simple_trivia.lang.fr.txt";
 const std::string LANG_FR_DATA_CONVERSATION_PROMPTS = "data.conversation_prompts.lang.fr.txt";
 
