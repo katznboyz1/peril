8acefc394d21f80fa40bdf89e2272a93cdc45497
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-24 04:17:04 +0000

    added an error code for when if(curl) does not evaluate to true

--- cpp_src/fetch_data_LANG_FR.cpp
+++ cpp_src/fetch_data_LANG_FR.cpp
@@ -45,7 +45,8 @@ int fetch_data_LANG_FR_all(const std::string &category_data_path, const std::str
         fclose(file);
 
         if (res != CURLE_OK) return 1;
-    }
+        
+    } else return 2;
 
     return 0;
 }
