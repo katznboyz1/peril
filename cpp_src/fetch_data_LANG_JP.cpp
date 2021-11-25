0c93d44576142bb2c3d053899b0a4dda428529d8
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-25 17:04:36 -0500

    finished all the stuff I can do now, will continue later

--- cpp_src/fetch_data_LANG_JP.cpp
+++ cpp_src/fetch_data_LANG_JP.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <curl/curl.h>
 #include <sqlite3.h>
-#include <zlib.h>
 
 #include "stdfunctions.hpp"
 
@@ -10,6 +9,8 @@
 using namespace std;
 
 int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::string &tmp_download_path, const int &curl_timeout_seconds) {
+
+    // forgive me
 
     CURL *curl_hiragana;
     CURL *curl_katakana;
@@ -27,12 +28,26 @@ int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::str
     std::string output_path_greetings = tmp_download_path + "LANG_JP_5.zip";
     std::string output_path_phrases = tmp_download_path + "LANG_JP_6.zip";
 
+    std::string output_path_hiragana_db = tmp_download_path + "LANG_JP_1.db";
+    std::string output_path_katakana_db = tmp_download_path + "LANG_JP_2.db";
+    std::string output_path_kanji_db = tmp_download_path + "LANG_JP_3.db";
+    std::string output_path_words_db = tmp_download_path + "LANG_JP_4.db";
+    std::string output_path_greetings_db = tmp_download_path + "LANG_JP_5.db";
+    std::string output_path_phrases_db = tmp_download_path + "LANG_JP_6.db";
+
     char* output_path_hiragana_char = const_cast<char*>(output_path_hiragana.c_str());
     char* output_path_katakana_char = const_cast<char*>(output_path_katakana.c_str());
     char* output_path_kanji_char = const_cast<char*>(output_path_kanji.c_str());
     char* output_path_words_char = const_cast<char*>(output_path_words.c_str());
     char* output_path_greetings_char = const_cast<char*>(output_path_greetings.c_str());
     char* output_path_phrases_char = const_cast<char*>(output_path_phrases.c_str());
+
+    char* output_path_hiragana_char_db = const_cast<char*>(output_path_hiragana_db.c_str());
+    char* output_path_katakana_char_db = const_cast<char*>(output_path_katakana_db.c_str());
+    char* output_path_kanji_char_db = const_cast<char*>(output_path_kanji_db.c_str());
+    char* output_path_words_char_db = const_cast<char*>(output_path_words_db.c_str());
+    char* output_path_greetings_char_db = const_cast<char*>(output_path_greetings_db.c_str());
+    char* output_path_phrases_char_db = const_cast<char*>(output_path_phrases_db.c_str());
 
     curl_hiragana = curl_easy_init();
     curl_katakana = curl_easy_init();
@@ -65,9 +80,10 @@ int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::str
         
 
         sqlite3* db_hiragana;
-        int res_hiragana = sqlite3_open("databaseName.db", &db_hiragana);
+        int res_hiragana = sqlite3_open(output_path_hiragana_char_db, &db_hiragana);
         if (!res_hiragana) {
 
+            // fetch the needed data
         }
         sqlite3_close(db_hiragana);
     }
