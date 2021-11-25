e6bb46fa19e4ac9fc7866a0b0049bbb1c8c3aeab
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-24 19:17:23 -0500

    the rows are somewhat populating themselves now

---
+++ web_src/custom.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+
+    let category_row_original = document.getElementsByClassName('peril-main-grid-row-category-cost')[0];
+
+    // let this auto-scale by referencing the "api" in the future
+    let num_of_rows = 5;
+    let num_of_categories = 6; 
+
+    for (i = 1; i < num_of_rows; i++) {
+
+        // clone the node (deep)
+        let category_duplicate = category_row_original.cloneNode(true);
+
+        document.getElementById('peril-main-board').appendChild(category_duplicate);
+    }
+
+    for (i = 0; i < num_of_rows; i++) {
+
+        let current_parent_node = document.getElementsByClassName('peril-main-grid-row-category-cost')[i];
+
+        for (j = 0; j < num_of_categories; j++) {
+
+            current_parent_node.children[j].children[0].innerHTML = "0000";
+        }
+    }
+}
