a255eb631838084685f65dcc8b3316c4b659ec6a
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-25 16:49:02 -0500

    1/6 japanese language trivia files are downloaded

--- cpp_src/fetch_data_LANG_JP.cpp
+++ cpp_src/fetch_data_LANG_JP.cpp
@@ -10,5 +10,56 @@ using namespace std;
 
 int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::string &tmp_download_path, const int &curl_timeout_seconds) {
 
+    CURL *curl_hiragana;
+    CURL *curl_katakana;
+    CURL *curl_kanji;
+    CURL *curl_words;
+    CURL *curl_greetings;
+    CURL *curl_phrases;
+
+    CURLcode res;
+
+    std::string output_path_hiragana = tmp_download_path + "LANG_JP_1.zip";
+    std::string output_path_katakana = tmp_download_path + "LANG_JP_2.zip";
+    std::string output_path_kanji = tmp_download_path + "LANG_JP_3.zip";
+    std::string output_path_words = tmp_download_path + "LANG_JP_4.zip";
+    std::string output_path_greetings = tmp_download_path + "LANG_JP_5.zip";
+    std::string output_path_phrases = tmp_download_path + "LANG_JP_6.zip";
+
+    char* output_path_hiragana_char = const_cast<char*>(output_path_hiragana.c_str());
+    char* output_path_katakana_char = const_cast<char*>(output_path_katakana.c_str());
+    char* output_path_kanji_char = const_cast<char*>(output_path_kanji.c_str());
+    char* output_path_words_char = const_cast<char*>(output_path_words.c_str());
+    char* output_path_greetings_char = const_cast<char*>(output_path_greetings.c_str());
+    char* output_path_phrases_char = const_cast<char*>(output_path_phrases.c_str());
+
+    curl_hiragana = curl_easy_init();
+    curl_katakana = curl_easy_init();
+    curl_kanji = curl_easy_init();
+    curl_words = curl_easy_init();
+    curl_greetings = curl_easy_init();
+    curl_phrases = curl_easy_init();
+
+    if (curl_hiragana && curl_katakana && curl_kanji && curl_words && curl_greetings && curl_phrases) {
+
+        FILE *file_hiragana;
+        FILE *file_katakana;
+        FILE *file_kanji;
+        FILE *file_words;
+        FILE *file_greetings;
+        FILE *file_phrases;
+
+        // full credit to the author: https://ankiweb.net/shared/info/1552733819
+        file_hiragana = fopen(output_path_hiragana_char, "wb");
+        curl_easy_setopt(curl_hiragana, CURLOPT_FAILONERROR, 1);
+        curl_easy_setopt(curl_hiragana, CURLOPT_URL, "https://dl5.ankiweb.net/shared/downloadDeck2/1552733819?k=WzE1NTI3MzM4MTksIDIzODEwLCAxNjMwNTYxOTU0XQ.ZARuox_JI2NSUp3tf-P-H-OWkFeXaajNlLAY9AlG5r8");
+        curl_easy_setopt(curl_hiragana, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl_hiragana, CURLOPT_WRITEDATA, file_hiragana);
+        curl_easy_setopt(curl_hiragana, CURLOPT_TIMEOUT, curl_timeout_seconds);
+        res = curl_easy_perform(curl_hiragana);
+        curl_easy_cleanup(curl_hiragana);
+        if (res != CURLE_OK) return 1;
+    }
+
     return 0;
 }
