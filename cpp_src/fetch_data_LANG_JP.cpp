c35dbcdd615847e98bf809c8d8ffdcb74402ac2b
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-26 12:04:35 -0500

    finally got fetch_data_LANG_JP_all to work

--- cpp_src/fetch_data_LANG_JP.cpp
+++ cpp_src/fetch_data_LANG_JP.cpp
@@ -3,8 +3,6 @@
 #include <sqlite3.h>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <boost/regex/icu.hpp>
 
 #include "stdfunctions.hpp"
 
@@ -13,8 +11,6 @@
 using namespace std;
 
 int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::string &tmp_download_path, const int &curl_timeout_seconds) {
-
-    std::wstring hiragana_all_chars = L"あいうえおかきくけこがきぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほばびぶべぼぱぴぷぺぽまみむめもやゆよらりるれろわん";
 
     CURL *curl[6];
     for (int i = 0; i < 6; i++) curl[i] = curl_easy_init();
@@ -27,7 +23,7 @@ int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::str
         FILE *files[6];
 
         std::string urls[6] = {
-            "https://gist.githubusercontent.com/katznboyz1/9c679680ca7e661a5bc6ae8fc2062bc9/raw/e1beecbdeba1006dd9c62543a687adc544727ab3/anki-1552733819.csv",
+            "https://gist.githubusercontent.com/katznboyz1/9c679680ca7e661a5bc6ae8fc2062bc9/raw/af616a91e36a0fef9e62115a49aa63c6a6c5268b/anki-1552733819.csv",
             "about:blank",
             "about:blank",
             "about:blank",
@@ -62,35 +58,23 @@ int fetch_data_LANG_JP_all(const std::string &category_data_path, const std::str
 
             std::ifstream anki_db_csv_input_file(tmp_download_path + output_files[i]);
             std::string line;
-            wofstream anki_db_csv_output_file(category_data_path + output_files[i]);
+            ofstream anki_db_csv_output_file;
+            anki_db_csv_output_file.open(category_data_path + output_files[i]);
             int current_line = 0;
 
             while (std::getline(anki_db_csv_input_file, line)) {
 
                 if (current_line != 0) {
                     
-                    std::vector<std::string> split_line = string_split(line, '|');
-                    std::string question_string_tmp = split_line[split_line.size() - 4];
-                    std::wstring question_string = std::wstring(question_string_tmp.begin(), question_string_tmp.end());
-                    std::string answer_string_tmp = split_line[split_line.size() - 3];
-                    std::wstring answer_string = std::wstring(answer_string_tmp.begin(), answer_string_tmp.end());
-                    std::wstring japanese_char;
-                    boost::u32regex japanese_char_regex = boost::make_u32regex(L"[ぁ-ゔ]");
-                    
-                    // the files quite literally have a char length of 5666666666666666666666666667766676666666665555566666666666666666666666660 for this index
-                    // so I will need to manually find japanese chars    
-                    boost::wsmatch matchU32;
-                    if (boost::u32regex_search(question_string, matchU32, japanese_char_regex)) {
+                    std::vector<std::string> split_line = readCSVRow(line);
 
-                        std::cout << "a" << endl;
-                        japanese_char = matchU32.str();
-                    }
-
-                    anki_db_csv_output_file << japanese_char << "|" << answer_string << "\n";
+                    anki_db_csv_output_file << split_line[split_line.size() - 5] << "|" << split_line[split_line.size() - 4] << "\n";
                 }
 
                 current_line++;
             }
+
+            anki_db_csv_output_file.close();
         }
     }
 
